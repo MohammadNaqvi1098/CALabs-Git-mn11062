ff66a5a7b0e040d31c12cefeddd0ec1c8c3ee8ef
MohammadNaqvi1098 <mn11062@st.habib.edu.pk>
2026-09-09 21:23:56 +0500

    Update Task_4.s

--- Task_4.s
+++ Task_4.s
@@ -9,12 +9,16 @@ main:
     li x6, 0x1F0
 
     sw x10, 0(x5) # store x10 at address in x5
+
+
     sw x11, 0(x6) # store x11 at address in x6
 
     lhu x12, 0(x5) # load word from address in x5 to x12
+
+
     lh x13, 0(x6) # load word from address in x6
 
-    lb x14, 0(x5) # load byte from address in x5 to x14
+    lb x14, 0(x6) # load byte from address in x6 to x14
 
 
 
