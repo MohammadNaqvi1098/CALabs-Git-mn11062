7a67f35140985b4abbb709e8288be5c942f87f7a
MohammadNaqvi1098 <mn11062@st.habib.edu.pk>
2026-09-16 20:10:53 +0500

    Refactor assembly code for nested loops and indexing

--- Lab_02_Task_4.s
+++ Lab_02_Task_4.s
@@ -1,31 +1,31 @@
 .text
 .globl main
 main:
-li x10, 0  # x10 = i
-li x9, 10   #  x9 = j
-li x11, 10   #  x11 = a
-li x12, 0    # x12 = b
-li x13, 0    # x13 = D
-
-li x4, 4
-li , x5, 0 # temporary
-
+ li x7, 0  # x7 = i
+li x29, 0   #  x29 = j
+li x5, 2  #  x5 = a
+li x6, 5    # x6 = b
+li x10, 0x100    # x10 = D
+li x4, 4 #temporary
+li x8, 0 # temporary
 For1:
-    bge x10,  x11, Exit1  # if a >= j, exit loop\
+    bge x7,  x5, Exit  # if a >= j, exit loop\
 
     For2:
-        bge x9 ,x12 Exit2  # if b >= j, exit loop
+        bge x29 ,x6 ,Exit1      # if b >= j, exit loop
 
-        mul x14, x9, x4
-        slli x14, x9 , 2  # j *4
-        add x14, x14, x13 # x14 = D + j*4
-        add x5, x10, x9 # x5 = i + j
-        sw x5, 0(x14)    # a[j] = i + j
+        slli x14, x29 , 4  # j *16
+        add x14, x14, x10 # x14 = D + j*16
+        add x9, x7, x29 # x9 = i + j
+        sw x9, 0(x14)    # a[j] = i + j
     
-    Exit2:
-    addi x9, x9, 1  # j++       
+    addi x29, x29, 1  # j++       
     beq x0 , x0 , For2  # go back to For2
-
 Exit1:
-    addi x10, x10, 1  # i++
+    addi x7, x7, 1  # i++
+    li x29, 0   #  j=0
     beq x0 , x0 , For1  # go back to For1
+Exit:
+end:
+j end
+
