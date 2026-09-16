16ad22193a43530c8580eefe3448d34eb0382966
MohammadNaqvi1098 <mn11062@st.habib.edu.pk>
2026-09-16 20:10:34 +0500

    Update Lab_02_Task_3.s

--- Lab_02_Task_3.s
+++ Lab_02_Task_3.s
@@ -1,45 +1,33 @@
 .text
 .globl main
 main:
-
-
-li x10 , 0  # x10 = i
-li x11, 0x100   #  x11 = a 
-li x12, 0    # x12 = sum
-
-li x20 , 10 
+li x22 , 0  # x22 = i
+li x11, 0x200   #  x11 = a 
+li x23, 0    # x23 = sum
+li x10 , 10 
 For:
-    bge x10 , x20 , Exit  # if i >= 10, exit loop
-
-    slli x13, x10, 2  # i *4
+    bge x22 , x10 , Exit  # if i >= 10, exit loop
+    slli x13, x22, 2  # i *4
     add x13, x13, x11 # x13 = a + i*4
-    sw x10 , 0(x13)    # a[i] = i
-
-    addi x10, x10, 1  # i++
+    sw x22 , 0(x13)    # a[i] = i
+    addi x22, x22, 1  # i++
     j For  # go back to For 
 
 Exit:
-
-    
-li x10 , 0  # x10 = i
-li x11, 0x100   #  x11 = a 
-li x12, 0    # x12 = sum
-
-li x20 , 10 
-
+li x22 , 0  # x22 = i
+li x11, 0x200   #  x11 = a 
+li x23, 0    # x23 = sum
+li x10 , 10 
 For2:
-    bge x10 , x20 , Exit2  # if i >= 10, exit loop
-
-    slli x13, x10, 2  # i *4
+    bge x22 , x10 , Exit2  # if i >= 10, exit loop
+    slli x13, x22, 2  # i *4
     add x13, x13, x11 # x13 = a + i*4
-    sw x13 , 0(x10)    # a[i] = i
-
-    add x12, x12, x10  # sum += a[i]
-    addi x10, x10, 1  # i++
+    lw x20 , 0(x13)    # x20 = a[i]
+    add x23, x23, x20  # sum += a[i]
+    addi x22, x22 , 1  # i++
     j  For2  # go back to For2
-
 Exit2:
     # Exit label for the second loop        
-
 end:
     j end
+
